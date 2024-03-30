9d2c398240e9f1abf2d76552d74e26c97682cc52
Ruth <ruthtang0807@gmail.com>
2024-03-30 21:56:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from reportlab.lib.utils import ImageReader
 from io import BytesIO
 from nltk.corpus import stopwords
 nltk.download('stopwords')
+import math
 
 st.set_page_config(page_title="Sentiment Analysis Web App", page_icon=":bar_chart:",layout="wide")
 
@@ -44,7 +45,7 @@ if authentication_status:
     # navigation menu
     selected = option_menu(
         menu_title=None,
-        options=["Home", "Text Analysis", "Contact", "Logout"],
+        options=["Home", "Comments Analyser", "Contact", "Logout"],
         icons=["house", "search", "envelope", "envelope"],
         menu_icon="cast",
         default_index=0,
@@ -53,14 +54,14 @@ if authentication_status:
 
     if selected == "Home":
         st.title(f"{selected}")
-    if selected == "Text Analysis":
+    if selected == "Comments Analyser":
         st.title(f"{selected}")
     if selected == "Contact":
         st.title(f"{selected}")
     if selected == "Logout":
         authenticator.logout("Logout", "main")
 
-    if selected == "Text Analysis":
+    if selected == "Comments Analyser":
 
         # Function to perform sentiment analysis
         def perform_sentiment_analysis(text):
@@ -87,6 +88,59 @@ if authentication_status:
                 if df[column].dtype == 'object':  # Check if column contains textual data
                     df['Polarity'], df['Sentiment'] = zip(*df[column].apply(perform_sentiment_analysis))
             return df
+        
+        ########################### generate report ###########################
+
+        def generate_wordcloud(clean_text):
+            wordcloud = WordCloud().generate(clean_text)
+            # Save WordCloud as an image
+            img_buffer = BytesIO()
+            plt.imshow(wordcloud, interpolation='bilinear')
+            plt.axis("off")
+            plt.savefig(img_buffer, format='png')
+            img_buffer.seek(0)
+            return img_buffer
+
+        def generate_pdf_report(overall_score, sentiment_counts, wordcloud_buffer, word_frequencies):
+            buffer = BytesIO()
+            pdf = canvas.Canvas(buffer, pagesize=letter)
+            page_width, page_height = letter
+            
+            # Overall Sentiment Score
+            pdf.drawString(100, page_height - 50, f"Overall Sentiment Score: {overall_score}")
+            
+            # Percentage of Sentiments
+            pdf.drawString(100, page_height - 70, f"Percentage of Positive Sentiment: {sentiment_counts.get('Positive', 0)} %")
+            pdf.drawString(100, page_height - 90, f"Percentage of Negative Sentiment: {sentiment_counts.get('Negative', 0)} %")
+            pdf.drawString(100, page_height - 110, f"Percentage of Neutral Sentiment: {sentiment_counts.get('Neutral', 0)} %")
+            
+            # Word Cloud
+            wordcloud_img = ImageReader(wordcloud_buffer)  # Convert BytesIO to ImageReader
+            pdf.drawImage(wordcloud_img, 100, page_height - 300, width=400, height=300)
+
+            # Top Ten Words with Highest Frequency
+            pdf.drawString(100, page_height - 350, "Top Ten Words with Highest Frequency:")
+            y_position = page_height - 370
+            max_items_per_page = 30  # Adjust as needed
+            current_page = 1
+            start_index = 0
+
+            for index, (word, frequency) in enumerate(word_frequencies.items()):
+                if index % max_items_per_page == 0 and index > 0:
+                    # Start a new page after reaching the maximum items per page
+                    pdf.showPage()
+                    current_page += 1
+                    y_position = page_height - 50  # Adjust as needed
+                    pdf.drawString(100, y_position, f"Page {current_page}")
+
+                pdf.drawString(100, y_position, f"{word}: {frequency}")
+                y_position -= 20
+
+            pdf.save()
+            
+            buffer.seek(0)
+            return buffer
+
 
         # Main function to run the Streamlit app
         def main():
@@ -124,13 +178,31 @@ if authentication_status:
                     
                     # Overall Sentiment Score
                     overall_score = analyzed_df['Polarity'].mean()
-                    st.write("Overall Sentiment Score:", round(overall_score, 2))
+                    # st.write("Overall Sentiment Score:", round(overall_score, 2))
+                    overall_score_percentage = round(overall_score * 100, 2)
+                    fig, ax = plt.subplots(figsize=(8, 2))
+                    ax.barh(['Overall Sentiment Score'], [100], color='lightgray')  # Full percentage bar
+                    ax.barh(['Overall Sentiment Score'], [overall_score_percentage], color='blue')  # Score percentage bar
+                    ax.set_xlabel('Percentage')
+                    ax.set_title('Overall Sentiment Score')
+                    ax.set_xlim(0, 100)
+                    ax.invert_yaxis()  # Invert y-axis to have the bar extend from left to right
+                    ax.text(overall_score_percentage + 2, 0, f'{overall_score_percentage}%', va='center')
+                    ax.legend()
+                    st.pyplot(fig)
 
                     # Percentage of Positive/Negative/Neutral Sentiment
                     sentiment_counts = analyzed_df['Sentiment'].value_counts(normalize=True) * 100
-                    st.write("Percentage of Positive Sentiment:", round(sentiment_counts.get('Positive', 0), 2), "%")
-                    st.write("Percentage of Negative Sentiment:", round(sentiment_counts.get('Negative', 0), 2), "%")
-                    st.write("Percentage of Neutral Sentiment:", round(sentiment_counts.get('Neutral', 0), 2), "%")
+                    # st.write("Percentage of Positive Sentiment:", round(sentiment_counts.get('Positive', 0), 2), "%")
+                    # st.write("Percentage of Negative Sentiment:", round(sentiment_counts.get('Negative', 0), 2), "%")
+                    # st.write("Percentage of Neutral Sentiment:", round(sentiment_counts.get('Neutral', 0), 2), "%")
+                    labels = sentiment_counts.index
+                    sizes = sentiment_counts.values
+                    fig, ax = plt.subplots(figsize=(8, 6))
+                    ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
+                    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+                    ax.set_title('Percentage of Sentiment')
+                    st.pyplot(fig)
 
                     # Perform data cleaning on the text column
                     cleaned_column = analyzed_df[analyzed_df.columns[0]].apply(lambda x: cleantext.clean(x, clean_all=False, extra_spaces=True,
@@ -146,11 +218,25 @@ if authentication_status:
                     ax.imshow(wordcloud, interpolation='bilinear')
                     ax.axis("off")
                     st.pyplot(fig)
+                    wordcloud_buffer = generate_wordcloud(clean_text)
 
                     # Top Ten Words with Highest Frequency
                     word_frequencies = pd.Series(clean_text.split()).value_counts()[:10]
-                    st.write("Top Ten Words with Highest Frequency:")
-                    st.write(word_frequencies) 
+                    # st.write("Top Ten Words with Highest Frequency:")
+                    # st.write(word_frequencies) 
+                    words = word_frequencies.index.tolist()
+                    frequencies = word_frequencies.values.tolist()
+                    fig, ax = plt.subplots(figsize=(10, 6))
+                    bars = ax.bar(words, frequencies, color='skyblue')
+                    ax.set_xlabel('Words')
+                    ax.set_ylabel('Frequency')
+                    ax.set_title('Top Ten Words with Highest Frequency')
+                    for bar, freq in zip(bars, frequencies):
+                        ax.text(bar.get_x() + bar.get_width() / 2, bar.get_height(), str(freq), ha='center', va='bottom')
+                    ax.set_xticks(ax.get_xticks())
+                    plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
+                    plt.tight_layout()
+                    st.pyplot(fig)
 
                     # # Themes discovery
                     # # Split cleaned text into words
@@ -163,46 +249,6 @@ if authentication_status:
                     # st.write("Top Themes with Frequency:")
                     # for theme, frequency in top_themes:
                     #     st.write(f"{theme}: {frequency}")                 
-
-                    ########################### generate report ###########################
-
-                    def generate_wordcloud(clean_text):
-                        wordcloud = WordCloud().generate(clean_text)
-                        # Save WordCloud as an image
-                        img_buffer = BytesIO()
-                        plt.imshow(wordcloud, interpolation='bilinear')
-                        plt.axis("off")
-                        plt.savefig(img_buffer, format='png')
-                        img_buffer.seek(0)
-                        return img_buffer
-
-                    def generate_pdf_report(overall_score, sentiment_counts, wordcloud_buffer, word_frequencies):
-                        buffer = BytesIO()
-                        pdf = canvas.Canvas(buffer, pagesize=letter)
-                        
-                        # Overall Sentiment Score
-                        pdf.drawString(100, 750, f"Overall Sentiment Score: {overall_score}")
-                        
-                        # Percentage of Sentiments
-                        pdf.drawString(100, 730, f"Percentage of Positive Sentiment: {sentiment_counts.get('Positive', 0)} %")
-                        pdf.drawString(100, 710, f"Percentage of Negative Sentiment: {sentiment_counts.get('Negative', 0)} %")
-                        pdf.drawString(100, 690, f"Percentage of Neutral Sentiment: {sentiment_counts.get('Neutral', 0)} %")
-                        
-                        # Word Cloud
-                        wordcloud_img = ImageReader(wordcloud_buffer)  # Convert BytesIO to ImageReader
-                        pdf.drawImage(wordcloud_img, 100, 500, width=400, height=300)
-
-                        # Top Ten Words with Highest Frequency
-                        pdf.drawString(100, 450, "Top Ten Words with Highest Frequency:")
-                        y_position = 430
-                        for word, frequency in word_frequencies.items():
-                            pdf.drawString(100, y_position, f"{word}: {frequency}")
-                            y_position -= 20
-                        
-                        pdf.save()
-                        
-                        buffer.seek(0)
-                        return buffer
 
                     # Generate the WordCloud
                     wordcloud_buffer = generate_wordcloud(clean_text)
